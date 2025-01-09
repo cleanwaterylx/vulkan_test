d0c01f760431d2d0e86318c9fcf2e0fc7ab23618
cleanwaterylx <109544465+cleanwaterylx@users.noreply.github.com>
2025-01-09 13:28:19 +0800

    3d-party

--- src/HelloTriangleApplication.hpp
+++ src/HelloTriangleApplication.hpp
@@ -4,6 +4,7 @@
 #include <vulkan/vulkan.h>
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/hash.hpp>
